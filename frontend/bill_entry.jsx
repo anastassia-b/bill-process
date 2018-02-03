3946475846353521ab7354e4b495f799d350f554
Anastassia Bobokalonova <email.anastassia@gmail.com>
2018-02-03 14:57:35 -0800

    Add root component

--- frontend/bill_entry.jsx
+++ frontend/bill_entry.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
 
   const root = document.getElementById('root');
 
-  ReactDOM.render(<h1>Billing App!</h1>, root);
+  ReactDOM.render(<Root store={ store } />, root);
 });
---
+++ frontend/components/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const App = () => (
+  <main className="main">
+    Welcome
+  </main>
+);
+
+export default App;
