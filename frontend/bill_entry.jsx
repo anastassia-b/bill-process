b8b484f412bebafd2b53191e6faef3adfbf5d55a
Anastassia Bobokalonova <email.anastassia@gmail.com>
2018-02-03 14:11:13 -0800

    Configure store with root reducer

--- frontend/bill_entry.jsx
+++ frontend/bill_entry.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+
   const root = document.getElementById('root');
+
   ReactDOM.render(<h1>Billing App!</h1>, root);
 });
