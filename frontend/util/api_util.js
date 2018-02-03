a1fb21282463052c1eb8dda50c12fdc114e7c836
Anastassia Bobokalonova <email.anastassia@gmail.com>
2018-02-03 15:03:50 -0800

    Add session ajax requests

---
+++ frontend/util/api_util.js
@@ -0,0 +1,29 @@
+export const login = user => (
+  $.ajax({
+      method: 'POST',
+      url: '/api/session',
+      data: { user }
+  })
+);
+
+export const signup = user => (
+  $.ajax({
+      method: 'POST',
+      url: '/api/users',
+      data: { user }
+  })
+);
+
+export const logout = () => (
+  $.ajax({
+      method: 'DELETE',
+      url: '/api/session',
+  })
+);
+
+export const fetchUser = id => (
+  $.ajax({
+      method: 'GET',
+      url: `/api/users/${id}`,
+  })
+);
