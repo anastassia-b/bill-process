f1d259fa7070e58cbc60c9c3ffac4ba21a67cb1a
Anastassia Bobokalonova <email.anastassia@gmail.com>
2018-02-04 10:38:12 -0800

    Fix reset session token call

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,13 +15,13 @@ class ApplicationController < ActionController::Base
   end
 
   def login(user)
-    user.reset_session_token
+    user.reset_session_token!
     session[:session_token] = user.session_token
     @current_user = user
   end
 
   def logout
-    current_user.reset_session_token
+    current_user.reset_session_token!
     session[:session_token] = nil
     @current_user = nil
   end
