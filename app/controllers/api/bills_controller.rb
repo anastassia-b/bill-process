1f2d21839805bbac9e24e066b53cddbab4d1bfc5
Anastassia Bobokalonova <email.anastassia@gmail.com>
2018-02-05 02:24:20 -0800

    Add create bill and bill action methods

--- app/controllers/api/bills_controller.rb
+++ app/controllers/api/bills_controller.rb
@@ -5,10 +5,41 @@ class Api::BillsController < ApplicationController
 
   def show
     @bill = Bill.find_by(id: params[:id])
-    @bill_actions = @bill.bill_actions
   end
 
   def create
-    #validations need to go here
+    # calculations of the bills amount could be done/validated on the backend or front
+    # validations need to go here, that its account role is finance
+
+    @bill = Bill.new(bill_params)
+    @bill.status = "DRAFT"
+
+    if @bill.save
+      # use params[:stakeholder_id]
+      # also create Bill Action in addition to creating a bill
+      @bill_action = BillAction.new(
+        bill_id: @bill.id,
+        stakeholder_id: params[:bill][:stakeholder_id],
+        comment: params[:bill][:comment],
+        action: "DRAFT"
+      )
+
+      if @bill_action.save
+        render 'api/bills/show'
+      else
+        Bill.last.destroy
+      end
+
+    else
+      render json: @bill.errors.full_messages, status: 422
+    end
+  end
+
+  private
+  # whitelist params
+  def bill_params
+    params.require(:bill).permit(
+      :customer_id, :month, :year, :overage_units, :overage_unit_cost, :overage_amount
+    )
   end
 end
