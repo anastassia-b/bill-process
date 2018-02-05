ebcf0ee4052a5ad36618b4f9e64b3550a6b1f765
Anastassia Bobokalonova <email.anastassia@gmail.com>
2018-02-05 02:25:34 -0800

    Create protected FinanceRoute

--- frontend/components/dashboard.jsx
+++ frontend/components/dashboard.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { FinanceRoute } from '../util/role_util';
 import LeftNavContainer from './leftnav/leftnav_container';
 import CustomerIndexContainer from './customers/customer_index_container';
 import CustomerShowContainer from './customers/customer_show_container';
 import BillIndexContainer from './bills/bill_index_container';
 import BillShowContainer from './bills/bill_show_container';
+import BillFormContainer from './bills/bill_form_container';
 
 const Dashboard = () => (
   <section className="dashboard-container">
     <LeftNavContainer />
     <div className="dashboard">
+      <FinanceRoute exact path="/dashboard" component={BillFormContainer} />
       <Route exact path="/dashboard" component={BillIndexContainer} />
       <Route exact path="/bill/:billId" component={BillShowContainer} />
       <Route exact path="/customers" component={CustomerIndexContainer} />
---
+++ frontend/util/role_util.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Route, Redirect, withRouter } from 'react-router-dom';
+
+const Finance = ({component: Component, path, role, exact}) => (
+  <Route path={path} exact={exact} render={(props) => (
+    (role === "Finance") ? (
+      <Component {...props}/>
+    ) : (
+      <div></div>
+    )
+  )}/>
+);
+
+
+const mapStateToProps = state => {
+  return {role: state.session.currentUser.role};
+};
+
+export const FinanceRoute = withRouter(
+  connect(mapStateToProps, null)(Finance)
+);
