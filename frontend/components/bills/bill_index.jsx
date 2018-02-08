2d44e95aceb731d9e19bdd2e50c1b4d58d8e258a
Anastassia Bobokalonova <email.anastassia@gmail.com>
2018-02-08 10:36:43 -0800

    Filter bill view based on status

--- frontend/components/bills/bill_index.jsx
+++ frontend/components/bills/bill_index.jsx
@@ -4,6 +4,10 @@ import { Link } from 'react-router-dom';
 class BillIndex extends React.Component {
   constructor(props) {
     super(props);
+
+    this.renderBills = this.renderBills.bind(this);
+    this.filterBills = this.filterBills.bind(this);
+    this.billDisplay = this.billDisplay.bind(this);
   }
 
   componentDidMount() {
@@ -11,25 +15,56 @@ class BillIndex extends React.Component {
     // this.props.fetchBills();
   }
 
-  render() {
+  componentWillReceiveProps(newProps) {
+    // later could implement fetching the correct bills from the backend.
+    // for now, filtering from all bills.
+  }
 
-    let billList = this.props.bills.map(bill => (
-      <Link to={`/bill/${bill.id}`} key={bill.id}>
-        <ul className="customer-item">
-          <li><h3>{bill.customer}: ${bill.overage_amount}</h3></li>
-          <li>Status: {bill.status}</li>
-          <li>Time Period: {bill.month} / {bill.year}</li>
-          <li>Overage Units: {bill.overage_units}</li>
-          <li>Overage Unit Cost: {bill.overage_unit_cost}</li>
-        </ul>
-      </Link>
-      )
-    );
+  renderBills(bills, status) {
+    let title = status.charAt(0).toUpperCase() + status.slice(1).toLowerCase();
+
+    let billList = bills.map(bill => (
+        <div className="bill-item" key={bill.id}>
+          <Link to={`/bills/${bill.id}`} key={bill.id}>
+          <ul>
+            <li><h3>{bill.customer}: ${bill.overage_amount}</h3></li>
+            <li>Status: {bill.status}</li>
+            <li>Time Period: {bill.month} / {bill.year}</li>
+          </ul>
+          </Link>
+        </div>
+    ));
 
     return (
-      <div className="customer-index-container">
-        <h2>Bills</h2>
+      <div className="bill-index-container">
+        <h2>{title} Bills</h2>
         {billList}
+      </div>
+    );
+  }
+
+  filterBills(status) {
+    let billStatus = status.toUpperCase();
+    let filteredBills = this.props.bills.filter(bill => (
+      bill.status === billStatus
+    ));
+    return this.renderBills(filteredBills, status);
+  }
+
+  billDisplay() {
+    let status = this.props.status;
+
+    if (status) {
+      return (this.filterBills(status));
+    } else {
+      return (this.renderBills(this.props.bills, 'all'));
+    }
+  }
+
+  render() {
+    return (
+      <div className="bill-display-container">
+        {this.billDisplay()}
       </div>
     );
   }
--- frontend/components/bills/bill_index_container.js
+++ frontend/components/bills/bill_index_container.js
@@ -1,9 +1,11 @@
 import { connect } from 'react-redux';
 import BillIndex from './bill_index';
 import { fetchBills } from '../../actions/bill_actions';
+import { withRouter } from 'react-router-dom';
 
-const mapStateToProps = (state) => ({
-  bills: Object.values(state.bills)
+const mapStateToProps = (state, ownProps) => ({
+  bills: Object.values(state.bills),
+  status: ownProps.match.params.status
 });
 
 const mapDispatchToProps = (dispatch) => ({
@@ -11,4 +13,6 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(BillIndex);
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(BillIndex)
+);
