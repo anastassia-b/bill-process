c9b7da0a7ce9255491fe137a064e20b7866128b0
Anastassia Bobokalonova <email.anastassia@gmail.com>
2018-02-08 13:27:46 -0800

    Add nullState to bill and usage reducers

--- frontend/reducers/bill_reducer.js
+++ frontend/reducers/bill_reducer.js
@@ -2,12 +2,18 @@ import merge from 'lodash/merge';
 import { RECEIVE_BILLS, RECEIVE_BILL } from '../actions/bill_actions';
 import { RECEIVE_CUSTOMER } from '../actions/customer_actions';
 
-const billReducer = (state = {}, action) => {
+const nullState = {};
+
+const billReducer = (state = nullState, action) => {
   Object.freeze(state);
 
   switch (action.type) {
     case RECEIVE_CUSTOMER:
-      return action.payload.bills;
+      if (action.payload.bills) {
+        return action.payload.bills;
+      } else {
+        return nullState;
+      }
     case RECEIVE_BILLS:
       return action.bills;
     case RECEIVE_BILL:
--- frontend/reducers/usage_reducer.js
+++ frontend/reducers/usage_reducer.js
@@ -1,12 +1,18 @@
 import merge from 'lodash/merge';
 import { RECEIVE_CUSTOMER } from '../actions/customer_actions';
 
+const nullState = {};
+
 const usageReducer = (state = {}, action) => {
   Object.freeze(state);
 
   switch (action.type) {
     case RECEIVE_CUSTOMER:
-      return action.payload.usage;
+      if (action.payload.usage) {
+        return action.payload.usage;
+      } else {
+        return nullState;
+      }
     default:
       return state;
   }
