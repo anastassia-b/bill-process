0f0b791cf539370e781d274d11f433792292385d
Anastassia Bobokalonova <email.anastassia@gmail.com>
2018-02-08 10:33:43 -0800

    Refactor bill links by status

--- frontend/components/leftnav/leftnav.jsx
+++ frontend/components/leftnav/leftnav.jsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 class LeftNav extends React.Component {
   constructor(props) {
     super(props);
+
+    this.renderCreateBill = this.renderCreateBill.bind(this);
   }
 
   componentDidMount() {
@@ -11,22 +13,32 @@ class LeftNav extends React.Component {
     this.props.fetchCustomers();
   }
 
+  renderCreateBill() {
+    if (this.props.currentUser.role === "Finance") {
+      return (
+        <li><Link to={`/newBill`}><h2>Create New Bill</h2></Link></li>
+      );
+    } else {
+      return (<li></li>);
+    }
+  }
+
   render() {
     console.log(this.props);
     let bills = this.props.bills;
     let customers = this.props.customers;
 
-    let billLinkList = bills.map(bill => (
-      <li key={bill.id}>
-        <Link to={`/bills/${bill.id}`}>{bill.customer}: ${bill.overage_amount}</Link>
-      </li>
-    ));
+    // let billLinkList = bills.map(bill => (
+    //   <li key={bill.id}>
+    //     <Link to={`/bills/${bill.id}`}>{bill.customer}: ${bill.overage_amount}</Link>
+    //   </li>
+    // ));
 
     let customerLinkList = customers.map(customer => (
       <li key={customer.id}>
         <Link to={`/customers/${customer.id}`}>{customer.name}</Link>
       </li>
-    ))
+    ));
 
     return (
       <div className="leftnav-container">
@@ -34,12 +46,19 @@ class LeftNav extends React.Component {
           <Link to={`/dashboard`}>
             <h2>Dashboard</h2>
           </Link>
+            <ul>
+              {this.renderCreateBill()}
+            </ul>
         </div>
 
         <div className="links">
           <Link to={`/dashboard`}><h2>Bills</h2></Link>
             <ul>
-              {billLinkList}
+              <li><Link to={`/dashboard`}><h2>ALL</h2></Link></li>
+              <li><Link to={`/dashboard/draft`}><h2>DRAFT</h2></Link></li>
+              <li><Link to={`/dashboard/final`}><h2>FINAL</h2></Link></li>
+              <li><Link to={`/dashboard/sent`}><h2>SENT</h2></Link></li>
+              <li><Link to={`/dashboard/writeoff`}><h2>WRITEOFF</h2></Link></li>
             </ul>
         </div>
 
