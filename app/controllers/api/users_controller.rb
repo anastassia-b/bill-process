727e02387f789ab270c39cd8aeb237596c9adb4b
Anastassia Bobokalonova <email.anastassia@gmail.com>
2018-04-23 12:19:19 -0700

    Initialize server-side analytics

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,10 +5,20 @@ class Api::UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       login(@user)
+      Analytics.identify(
+        user_id: "#{@user.id}",
+        traits: { 
+          name: "#{@user.name }",
+          role: "#{@user.role }"
+        }
+        )
       render "api/users/show"
     else
       render json: @user.errors.full_messages, status: 422
     end
+
+    # For debugging:
+    # Analytics.identify( user_id: "#{user1.id}", traits: { name: "#{ user1.name }", role: "#{ user1.role }"})
   end
 
   def show
